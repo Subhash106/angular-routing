d14fe0e8eacbda4828475b94fab4a17e9c952b2d
Subash_Chandra <s.chandra106@gmail.com>
2023-11-10 09:12:36 +0530

    Added not found route and gaurds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,24 +10,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [{ path: ':id/:name', component: UserComponent }],
-  },
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ],
-  },
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGaurd } from './auth-gaurd.service';
 
 @NgModule({
   declarations: [
@@ -39,8 +24,8 @@ const appRoutes: Routes = [
     EditServerComponent,
     ServerComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthService, AuthGaurd],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/auth-gaurd.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGaurd implements CanActivate {
+  constructor(private authService: AuthService, private route: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.isAuthenticated().then((loggedIn: boolean) => {
+      if (loggedIn) {
+        return true;
+      }
+
+      this.route.navigate(['/']);
+    });
+  }
+}
