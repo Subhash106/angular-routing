38d52651090500de0d328a99f0a5a150788f1006
Subash_Chandra <s.chandra106@gmail.com>
2023-11-13 07:05:27 +0530

    learned rxjs basics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { interval, Observable, Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,10 +8,48 @@ import { AuthService } from '../auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private intervalSubscription: Subscription;
+
   constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.intervalSubscription = interval(1000).subscribe((count) => {
+    //   console.log('count', count);
+    // });
+
+    const customIntervalObservable = new Observable((subscriber) => {
+      let count: number = 0;
+      setInterval(() => {
+        subscriber.next(count);
+
+        if (count > 2) {
+          subscriber.complete();
+        }
+
+        if (count > 3) {
+          subscriber.error(new Error('Something went wrong!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.intervalSubscription = customIntervalObservable.subscribe(
+      (count: number) => {
+        console.log('count', count);
+      },
+      (error: Error) => {
+        console.log(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+  }
 
   loadServers(id: number) {
     this.router.navigate(['/servers', id, 'edit'], {
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -10,8 +11,12 @@ import { Subscription } from 'rxjs/Subscription';
 export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
   paramsSubscription: Subscription;
+  isActive: boolean = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.user = {
@@ -23,9 +28,21 @@ export class UserComponent implements OnInit, OnDestroy {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
+
+    this.userService.activateUser.subscribe((data: boolean) => {
+      this.isActive = data;
+    });
   }
 
   ngOnDestroy(): void {
     this.paramsSubscription.unsubscribe();
+  }
+
+  activateUser() {
+    this.userService.activateUser.next(true);
+  }
+
+  onDeactivate() {
+    this.userService.activateUser.next(false);
   }
 }
